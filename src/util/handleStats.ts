d4232641fbf9bdf17d5c5749d25bc289f0a8fe0e
PaulHan <zhangyu.han@outlook.com>
2022-07-25 14:21:51 +0800

    fix: handle stats error

--- src/util/handleStats.ts
+++ src/util/handleStats.ts
@@ -2,15 +2,30 @@ import chalk from 'chalk'
 import { loggerError, loggerInfo, loggerSuccess } from "@/util/index";
 import { Stats } from "webpack";
 import columnify from "columnify";
+import formatWebpackMessage from './formatWebpackMessage';
 
-export default function outputProductSize(stats: Stats) {
-  loggerInfo('****** OUTPUT START ******')
+export default function handleStats(stats: Stats) {
   try {
-    const assetInfo = stats.toJson({
+    loggerInfo('****** OUTPUT START ******')
+    const statsInfo = stats.toJson({
       all: false,
       assets: true,
+      warnings: true,
+      errors: true,
     });
-    const assets = assetInfo?.assets;
+
+    const messages = formatWebpackMessage(statsInfo);
+    if ((messages.errors || []).filter(Boolean).length) {
+      console.log(messages.errors?.[0]);
+      throw new Error('Failed to compile.');
+    }
+
+    if ((messages.warnings || []).filter(Boolean).length) {
+      console.log(chalk.yellow('Compiled with warnings.\n'));
+      console.log(messages.warnings?.join('\n\n'));
+    }
+
+    const assets = statsInfo?.assets;
     if (!assets) return;
     const columns = columnify(
       assets.map((d) => ({size: d.size, name: d.name})),
@@ -38,7 +53,8 @@ export default function outputProductSize(stats: Stats) {
     );
     console.log(columns);
     loggerSuccess(`total size: ${(assets.reduce((a: any, b: any) => a + b.size, 0) / 1000) | 0}KB cost: ${(stats.endTime - stats.startTime) / 1000}s`)
-  } catch (err) {
+  } catch (err: any) {
     loggerError('****** OUTPUT FAILED ******')
+    loggerError(err.message);
   }
 }
