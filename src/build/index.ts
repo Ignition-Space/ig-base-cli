5df4345d613175ece88547f941c70a7f00b4b88c
cookieBoty <cookieboty@qq.com>
2023-07-30 19:33:26 +0800

    feat: 添加前后流程hook

--- src/build/index.ts
+++ src/build/index.ts
@@ -5,7 +5,7 @@ import shelljs from 'shelljs'
 import TaskQueue from "@/util/taskQueue"
 import { loggerInfo } from "@/util"
 
-const buildFlow = () => {
+const buildFlow = async () => {
   const taskQueue = new TaskQueue()
   const exist = ConfigExist("ig.config.ts")
 
@@ -18,15 +18,36 @@ const buildFlow = () => {
   if (buildConfig.flow) {
     const { flow } = buildConfig
 
+    if (flow.preHook) {
+      loggerInfo(`stage.preHook==>${flow.preHook}`)
+      await shelljs.exec(flow.preHook)
+    }
+
     // Add task into queue
     flow?.stages.forEach((stage: any) => {
       if (flow[stage].script) {
-        loggerInfo(`stage.script==>+${flow[stage].script}`)
-        taskQueue.enqueue(() => {
-          shelljs.exec(flow[stage].script);
+        loggerInfo(`stage.script==>${flow[stage].script}`)
+        taskQueue.enqueue(async () => {
+
+          if (flow[stage].preHook) {
+            await shelljs.exec(flow[stage].preHook)
+          }
+
+          await shelljs.exec(flow[stage].script)
+
+          if (flow[stage].doneHook) {
+            await shelljs.exec(flow[stage].doneHook)
+          }
         })
       }
     })
+
+    if (flow.doneHook) {
+      taskQueue.enqueue(async () => {
+        loggerInfo(`stage.doneHook==>${flow.doneHook}`)
+        await shelljs.exec(flow.doneHook)
+      })
+    }
 
     // Execute queue
     taskQueue.start()
